b60662ace4e095d318d9348211257234583249b1
Anshu Kumar <anshukumar@Anshus-MacBook.local>
2023-08-06 00:41:42 +0530

    first commit

---
+++ mernapp/src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div><div className="container">
+    <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <div className="col-md-4 d-flex align-items-center">
+        <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+        </a>
+        <span className="text-muted">© 2023 <i>FlavorDrop</i>, Inc</span>
+      </div>
+  
+      <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+        <li className="ms-3"><a className="text-muted" href="/"><svg className="bi" width="24" height="24"><use ></use></svg></a></li>
+        <li className="ms-3"><a className="text-muted" href="/"><svg className="bi" width="24" height="24"><use ></use></svg></a></li>
+        <li className="ms-3"><a className="text-muted" href="/"><svg className="bi" width="24" height="24"><use></use></svg></a></li>
+      </ul>
+      <a href='https://www.linkedin.com/in/iakumar0132/' target="_blank" rel="noreferrer"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2RYeN56EvozwyyxYGDw4dTu-pbUZyNxnF93zSLUcOlQ&s" alt='' style={{ width: '40px', height: '40px', marginLeft: '160px' }}></img>
+</a>
+      <div className='dev text-muted'>Developed By Anshu Kumar</div>
+    </footer>
+  </div>
+  </div>
+  )
+}
